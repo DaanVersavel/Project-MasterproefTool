18bae6a70a7984314bf69052db4f3dffbd090509
DaanVersavel <92854110+DaanVersavel@users.noreply.github.com>
2022-02-22 13:12:31 +0100

    first init

---
+++ Backend/MasterproofTool/src/main/java/com/example/MasterproofTool/MasterproefToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.MasterproofTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterproefToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterproefToolApplication.class, args);
+	}
+
+}
---
+++ Backend/MasterproofTool/src/test/java/com/example/MasterproofTool/MasterproefToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MasterproofTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterproefToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
