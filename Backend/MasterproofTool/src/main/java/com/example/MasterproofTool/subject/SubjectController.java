49f180856c9366da3d805da080549041682e8308
DaanVersavel <92854110+DaanVersavel@users.noreply.github.com>
2022-04-09 18:05:01 +0200

    update to controllers

--- Backend/MasterproofTool/src/main/java/com/example/MasterproofTool/subject/SubjectController.java
+++ Backend/MasterproofTool/src/main/java/com/example/MasterproofTool/subject/SubjectController.java
@@ -1,6 +1,7 @@
 package com.example.MasterproofTool.subject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,10 +24,10 @@ public class SubjectController {
         subjectService.addNewSubject(subject);
     }
 
-    //get method for list of subject
+    //get method for list of subject that are approved
     @GetMapping
     @CrossOrigin(origins = "*")
-    public List<Subject> getSubjects(){ return subjectService.getSubjects();
+    public List<Subject> getSubjectsApproved(){ return subjectService.getSubjectsApproved();
     }
 
     //get method for not yet accepted methods
@@ -35,5 +36,23 @@ public class SubjectController {
     public List<Subject> getSubjectForReview(){
         return subjectService.getSubjectsForReview();
     }
+
+    //update method to assign true to subject
+    @PutMapping(path="/Review/{id}")
+    public ResponseEntity<Subject> updateSubjectReviewedTrue(@PathVariable long id){
+        return ResponseEntity.ok(subjectService.updateSubjectReviewedTrue(id));
+    }
+
+
+    //TODO doesn't work yet
+    //get method for getting your specific subjects
+    @GetMapping(path = "/MySubjects")
+    @CrossOrigin(origins = "https://localhost:3000")
+    public List<Subject> getSubjectForUser(@RequestBody long coordinator_id){
+        return subjectService.getSubjectForUser(coordinator_id);
+    }
+
+
+
 }
 
